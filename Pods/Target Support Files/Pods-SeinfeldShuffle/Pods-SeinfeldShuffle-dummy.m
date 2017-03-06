5e13b2196386a9dc1f66ae682b913bba8d2392c4
Robert Deans <robdeans@gmail.com>
2017-03-06 17:52:00 -0500

    adds podfile and installs snapkit

---
+++ Pods/Target Support Files/Pods-SeinfeldShuffle/Pods-SeinfeldShuffle-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SeinfeldShuffle : NSObject
+@end
+@implementation PodsDummy_Pods_SeinfeldShuffle
+@end
---
+++ Pods/Target Support Files/Pods-SeinfeldShuffle/Pods-SeinfeldShuffle-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_SeinfeldShuffleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SeinfeldShuffleVersionString[];
+
